3617891aed938e75088890fcb6332de74c37f4dd
chari <chari@DESKTOP-DR8C3OD>
2017-12-13 22:51:45 +0530

    Add project files.

---
+++ MVCCurd01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCCurd01.MvcApplication" Language="C#" %>
---
+++ MVCCurd01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCCurd01.Startup))]
+namespace MVCCurd01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
